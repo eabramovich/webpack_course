4d57be1323b4c7a5851ea7cfd908ee5bc6620d1a
Евгения Абрамович <ewg.abramovich2014@yandex.ru>
2024-10-07 14:21:42 +0300

    Add env variables, HtmlWebpackPlugin, ProgressPlugin, loaders, dev server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { calc } from "./test";
 
 console.log('Hello world!');
-console.log(calc(5, 7));
+console.log(calc(520 , 7));
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+export function calc(a: number, b: number) {
+  return a + b;
+}
+  
